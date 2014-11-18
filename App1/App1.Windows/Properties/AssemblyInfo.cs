22e2a3cee0b2fc6d423f076598503ae1b3a47f1d
Baranov Nikita <baranov.nikita@gmail.com>
2014-11-18 20:05:27 +0300

    Swooper.Windows => Swooper

--- App1/App1.Windows/Properties/AssemblyInfo.cs
+++ App1/App1.Windows/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Swooper.Windows")]
+[assembly: AssemblyTitle("Swooper")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Swooper.Windows")]
+[assembly: AssemblyProduct("Swooper")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
